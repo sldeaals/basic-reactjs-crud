74a63b7860a57c168dbcecc91fdc98071ea61a7c
sldeaals <danielariasaguero@gmail.com>
2024-06-02 20:33:37 -0400

    feat: create edit modal for procedures

---
+++ src/containers/index.tsx
@@ -0,0 +1,5 @@
+import ProceduresAlertPanel from "./ProceduresAlertPanel";
+import ProceduresEditModal from "./ProceduresEditModal";
+import ProceduresTable from "./ProceduresTable";
+
+export { ProceduresAlertPanel, ProceduresEditModal, ProceduresTable };
